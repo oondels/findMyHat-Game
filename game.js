039df1e43baeea71dfa49950ab06f2a645a32c19
oondels <hendriusfelix@gmail.com>
2024-04-25 00:38:08 -0300

    moving the player position

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")({ sigint: true });
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -12,47 +12,53 @@ const pathCharacter = "*";
 class Field {
   constructor(field) {
     this.field = field;
-  }
-  print() {
-    let joinField = "";
-    let fieldInfo = {
+    this.fieldInfo = {
       largura: 0,
       altura: 0,
     };
-    let objectsPositions = {
+    this.objectsPositions = {
       hat: [],
       holes: [],
+      player: [0, 0],
     };
+  }
+
+  getFieldInfo() {
+    return this.fieldInfo;
+  }
+
+  getObjectsPositions() {
+    return this.objectsPositions;
+  }
+
+  print() {
+    let joinField = "";
+
     for (let i = 0; i < this.field.length; i++) {
       if (i > 0) {
         joinField += "\n";
       }
-      // Altura do campo
-      fieldInfo.altura = this.field.length;
 
       for (let j = 0; j < this.field[i].length; j++) {
-        // Largura do campo
-        fieldInfo.largura = this.field[i].length;
-        joinField += this.field[i][j];
+        // Altura e Largura do campo
+        this.fieldInfo.altura = this.field.length;
+        this.fieldInfo.largura = this.field[i].length;
 
+        joinField += this.field[i][j];
         // Mapeando posição dos buracos e do chapéu
         switch (this.field[i][j]) {
           case "^":
-            objectsPositions.hat = [i, j];
+            this.objectsPositions.hat = [i, j];
             break;
           case "O":
-            objectsPositions.holes.push([i] + [j]);
+            this.objectsPositions.holes.push([i] + [j]);
             break;
           default:
             break;
         }
       }
     }
-    // Debug
-    console.log(
-      `Posição do Chapéu: ${objectsPositions.hat} e Posições dos Buracos: ${objectsPositions.holes}`
-    );
-    console.log(`Largura: ${fieldInfo.largura} e Altura: ${fieldInfo.altura}`);
+
     return joinField;
   }
 }
@@ -66,14 +72,38 @@ const newField = new Field([
 
 const playGame = (field, key = 1) => {
   field = newField.field;
-  while (key === 1) {
+
+  // Get user movement
+  const move = () => {
     readline.question("Where you go?", (choice) => {
       console.log(choice);
+
+      switch (choice.toLowerCase()) {
+        case "d":
+          newField.getObjectsPositions().player[1] += 1;
+          console.log(newField.getObjectsPositions().player);
+          break;
+        case "a":
+          newField.getObjectsPositions().player[1] -= 1;
+          console.log(newField.getObjectsPositions().player);
+          break;
+        case "w":
+          newField.getObjectsPositions().player[0] += 1;
+          console.log(newField.getObjectsPositions().player);
+          break;
+        case "s":
+          newField.getObjectsPositions().player[0] -= 1;
+          console.log(newField.getObjectsPositions().player);
+          break;
+
+        default:
+          console.log("Select a real value.");
+          break;
+      }
       readline.close();
-      // continue here
     });
-  }
+  };
+  move();
 };
 
-console.log(newField.print());
 playGame();
